b98248afc063d7bb8cb9b6c96b1b4f2fa29c2102
Quang Ho <157239q@gmail.com>
2022-08-05 00:22:28 -0400

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('k1a',
-                    sources=['src/demo.cpp'], language="c++")
+                    sources=['src/demo.cpp'], language="c++", extra_compile_args=["-O3"])
+
+with open("src/demo.cpp") as f:
+    version = [l for l in f.readlines(
+    ) if "*version" in l][0].split("=")[1].strip().strip(" ").strip(";").strip("\"")
 
 setup(name='k1a',
-      version='1.0',
+      version=version,
       description='Accelerated functionalities for k1lib',
       author="Quang Ho",
       author_email="157239q@gmail.com",
